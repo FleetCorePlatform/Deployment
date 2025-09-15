30e4188602974b7ca89cb2b71f3d872cc83bc78f
Panagiotis Bellias <belliaspan@gmail.com>
2025-09-15 16:57:58 +0300

    feat(ci): fix Terraform + Ansible deployment pipeline for FleetCoreServer (#1)

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -199,7 +199,7 @@ resource "aws_db_instance" "fleetcore_db" {
   engine               = "postgres"
   engine_version       = "13"
   instance_class       = "db.t3.micro"
-  name                 = var.rds_db_name
+  db_name              = var.rds_db_name
   username             = var.rds_username
   password             = var.rds_password
   skip_final_snapshot  = true
