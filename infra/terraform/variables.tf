1387dae04e4700cc98f7fd5d03cee423fef3b82b
Panagiotis Bellias <belliaspan@gmail.com>
2025-09-15 16:42:15 +0300

    feat(ci): fix Terraform + Ansible deployment pipeline for FleetCoreServer (#1)

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,29 +1,40 @@
+// Cloud provider
 variable "cloud_provider" {
   type    = string
   default = "aws"
 }
 
-variable "vpc_cidr_block" {
+// AWS region
+variable "aws_region" {
   type    = string
+  default = "us-east-1"
+}
+
+// Networking
+variable "vpc_cidr_block" {
+  type = string
 }
 
 variable "public_subnet_cidr" {
-  type    = string
+  type = string
 }
 
 variable "private_subnet_cidr" {
-  type    = string
+  type = string
 }
 
+// SSH key
 variable "ssh_key_name" {
-  type    = string
+  type = string
 }
 
+// Application source branch
 variable "app_branch" {
   type    = string
   default = "main"
 }
 
+// Instance settings
 variable "instance_type" {
   type    = string
   default = "t3.micro"
@@ -32,4 +43,35 @@ variable "instance_type" {
 variable "instance_count" {
   type    = number
   default = 1
+}
+
+// S3 artifacts
+variable "s3_bucket_name" {
+  type = string
+}
+
+variable "jar_s3_key" {
+  type    = string
+  default = "artifacts/FleetCoreServer.jar"
+}
+
+variable "lambda_s3_key" {
+  type    = string
+  default = "lambda/mission_end.zip"
+}
+
+// RDS settings
+variable "rds_db_name" {
+  type    = string
+  default = "fleetcore"
+}
+
+variable "rds_username" {
+  type    = string
+  default = "fleetcore"
+}
+
+variable "rds_password" {
+  type      = string
+  sensitive = true
 }
