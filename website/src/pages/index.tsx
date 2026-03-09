f9340be455acd96cdddaa33891b934186c65b7fe
Levy-Y <65444028+Levy-Y@users.noreply.github.com>
2026-03-09 22:13:34 +0100

    fix: Changed baseUrl to match the repository name

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Get Started - Documentation 🚀
+            Read the Documentation
           </Link>
         </div>
       </div>
